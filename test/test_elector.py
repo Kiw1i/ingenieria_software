9a4e20d9b6333cec533582a9d085331af6679afd
kerin larico <145733758+kiwigenio@users.noreply.github.com>
2024-12-26 09:07:43 -0500

    actualizacion  test_elector.py

--- test/test_elector.py
+++ test/test_elector.py
@@ -1,4 +1,8 @@
+import sys
+import os
 import pytest
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
 from Model.dominio.participantes.Elector import Elector
 
 @pytest.fixture
