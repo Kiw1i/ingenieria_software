6ce8c486d7d1cbd60b85c886d349a37c81343d8d
kerin larico <145733758+kiwigenio@users.noreply.github.com>
2024-12-27 09:41:38 -0500

    Update Elector.py

--- Model/dominio/participantes/Elector.py
+++ Model/dominio/participantes/Elector.py
@@ -1,43 +1,37 @@
 from Model.repositorio.MySQL.elector_repositorio_impl import elector_repositorio_impl
 from Model.models.Elector import Elector
 
-class Elector: 
+class Elector:
     def __init__(self, id, correo, contrasena, nombre, apellido):
         self.id = id
         self.correo = correo
-        self.contrasena = contrasena  
+        self.contrasena = contrasena
         self.nombre = nombre
         self.apellido = apellido
-        self.ha_votado = "No"  
+        self.ha_votado = False
 
     def registrar(self, nombre, apellido):
         self.nombre = nombre
         self.apellido = apellido
-        print(f"Registrado: {self.nombre} {self.apellido}") 
         return "Registro exitoso"
 
     def iniciar_sesion(self, correo, contrasena):
-      
         if self.correo == correo and self.contrasena == contrasena:
-            print("Inicio de sesión exitoso") 
-            print(f"Error: correo o contraseña incorrectos para {correo}")
             return "Inicio de sesión exitoso"
         else:
-            return f"Error: correo o contraseña incorrectos para {correo}" 
+            return "Correo o contraseña incorrectos"
 
     def votar(self):
-        if self.ha_votado == "No": 
+        if not self.ha_votado:
             self.ha_votado = True
-            print(f"Voto registrado para {self.nombre}") 
             return "Voto registrado"
         else:
             return "Ya ha votado"
 
     def estado_voto(self):
-      
-        return "Sí ha votado" if self.votado == "Yes" else "No ha votado"  
+        return "Ha votado" if self.ha_votado else "No ha votado"
 
     def editar_datos(self, nombre, apellido):
         self.nombre = nombre
         self.apellido = apellido
-        return f"Datos editados a: {self.nombre} {self.apellido}"  
+        return "Datos editados exitosamente"
